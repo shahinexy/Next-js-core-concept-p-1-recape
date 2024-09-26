532a9695d9b637f0234d508d5c65e56421267df8
shahinexy <shahinexy@gmail.com>
2024-09-26 23:22:01 +0600

    component created

---
+++ src/app/shope/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShopeContent from '../../components/ShopeContent';
+
+const shoepapp = () => {
+    return (
+        <div>
+            <ShopeContent/>
+        </div>
+    );
+};
+
+export default shoepapp;
---
+++ src/components/ShopeContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShopeContent = () => {
+    return (
+        <div>
+            <h1 className='text-green-400 text-3xl font-semibold'>This shope coentent coms from shopeContent component.</h1>
+        </div>
+    );
+};
+
+export default ShopeContent;
