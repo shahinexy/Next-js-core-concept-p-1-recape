9093cdb1c6446c2b0584ca19f860b4d11eac8be7
shahinexy <shahinexy@gmail.com>
2024-10-08 00:11:13 +0600

    dynamic navbar added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,38 @@
+"use client"
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
+    const navLinks= [
+        {
+            title: "Home",
+            slug: '/'
+        },
+        {
+            title: "Shop",
+            slug: "/shop"
+        },
+        {
+            title: "FAQ's",
+            slug: "faqs"
+        },
+        {
+            title: "About Us",
+            slug: "about-us"
+        },
+        {
+            title: "Contact Us",
+            slug: "/contact-us"
+        }
+    ]
+
     return (
         <div>
              <nav className="bg-green-800 flex justify-center py-2">
               <ul className="flex justify-center gap-5 text-white text-lg font-bold">
-                <li>
-                  Home
-                </li>
-                <li>
-                  Shop
-                </li>
-                <li>
-                  FAQ&apos;s
-                </li>
-                <li>
-                  About 
-                </li>
-                <li>
-                  Contact
-                </li>
+                {
+                    navLinks?.map((link) => <Link key={link.slug} href={link.slug}>{link.title}</Link>)
+                }
               </ul>
             </nav>
         </div>
