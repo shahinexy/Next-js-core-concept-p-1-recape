79084355abeb62ed78631f058febec3998b559bd
shahinexy <shahinexy@gmail.com>
2024-10-15 23:23:03 +0600

    about us page issue fixed

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ const aboutpage = () => {
     return (
         <div>
             <h1 className='text-5xl font-semibold text-red-800'>This is about us page</h1>
-            This is for the todays git push
+            
         </div>
     );
 };
